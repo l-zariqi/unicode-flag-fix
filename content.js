151100837326d5e4e1e6dd76a9cebedf48d8d0a1
l-zariqi <lind_zariqi@hotmail.com>
2025-11-05 17:08:09 +0000

    Better DOM replacement and parsing

--- content.js
+++ content.js
@@ -2,37 +2,33 @@ function replaceFlagEmojis() {
   const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
   let nodesToReplace = [];
 
-  // Regular expression to match flag emojis
-  const flagRegex = /[\u{1F1E6}-\u{1F1FF}]{2}/gu;
+  // Match regional indicator pairs (flags)
+  const flagRegex = /([\u{1F1E6}-\u{1F1FF}]{2})/gu;
 
   while (walker.nextNode()) {
     const node = walker.currentNode;
     let text = node.nodeValue;
 
-    // Check if the text contains any flag emoji
     if (flagRegex.test(text)) {
       nodesToReplace.push({ node, text });
     }
   }
 
   for (const { node, text } of nodesToReplace) {
-    // Create a new span to hold the modified content
-    const span = document.createElement("span");
-
-    // Use Twemoji to parse the text and replace flag emojis with images
-    span.innerHTML = twemoji.parse(text, {
-      folder: 'svg',
-      ext: '.svg',
-      className: 'twemoji',
-      size: '72x72',
+    const html = twemoji.parse(text, {
+      folder: "svg",
+      ext: ".svg",
+      className: "twemoji",
+      size: "72x72",
     });
 
-    // Replace the original node with the new span
-    node.replaceWith(span);
+    const fragment = document.createElement("span");
+    fragment.innerHTML = html;
+
+    node.replaceWith(...fragment.childNodes);
   }
 }
 
-// Run after page load
 window.onload = () => {
   replaceFlagEmojis();
   const observer = new MutationObserver(replaceFlagEmojis);
